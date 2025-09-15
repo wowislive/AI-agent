ba11b5d484d3384ec2db51a4e1804566d71aa02c
wowislive <wowislive@gmail.com>
2025-09-15 18:07:11 +0000

    prompt type and arguments updates

--- main.py
+++ main.py
@@ -1,20 +1,30 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 
-response = client.models.generate_content(
-    model="gemini-2.0-flash-001",
-    contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
-)
-
 def main():
+    if len(sys.argv) < 2:
+        sys.exit(1)
+    
+    user_prompt = sys.argv[1]
+    messages = [types.Content(role="user", parts=[types.Part(text=user_prompt)]),]
+    
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001",
+        contents=messages,)
+    
     print(response.text)
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    
+    if len(sys.argv) >= 3 and sys.argv[2] == "--verbose":
+        print(f"User prompt: {user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 if __name__ == "__main__":
     main()
